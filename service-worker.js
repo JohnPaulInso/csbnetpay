c3c1756ede45879ee83bf28f860adc1b0e6e62d7
JohnPaulInso <88543202+JohnPaulInso@users.noreply.github.com>
2026-01-06 18:52:54 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,44 @@
+const CACHE_NAME = "csb-search-v1";
+const ASSETS = [
+  "./",
+  "index.html",
+  "index2.html",
+  "index3.html",
+  "index4.html",
+  "index5.html",
+  "index.css",
+  "styles.css",
+  "index.script",
+  "index2.script",
+  "index3.script",
+  "index4.script",
+  "manifest.json",
+  "version.json",
+  "citysavings_logo.png",
+  "favicon.png",
+  "loading.gif",
+  "listening.ogg",
+  "searching.ogg"
+];
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("csb-netpay").then(cache => {
-      return cache.addAll(["login/index.html"]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
