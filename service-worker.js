7bac9dd59431a79e20568943c76a005ce92c1238
John Paul Inso <john.paulinso@fusionos.ai>
2026-07-21 20:16:56 +0800

    Update: 07 21 2026  08:16

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@
  * Fix: Bumped cache version to v18.
  *      Forces browsers to reload updated script files reverting fetch requests to raw CSV files directly.
  */
-/* (2026-07-13) Bump cache version to v27; prev: v26 */
-const CACHE_NAME = "csb-search-v27";
+/* (2026-07-13) Bump cache version to v28; prev: v27 */
+const CACHE_NAME = "csb-search-v28";
 const ASSETS = [
   "./",
   "index.html",
